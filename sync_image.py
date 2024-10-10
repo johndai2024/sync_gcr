62dbc2d0c642d090b024331de755736b5498ca9a
johndai2024 <4644535@qq.com>
2024-10-10 15:18:27 +0800

    Update sync_image.py
    add cpu

--- sync_image.py
+++ sync_image.py
@@ -23,16 +23,16 @@ def pull_image():
             tag = sha256_name[-1].split(":")[-1][0:6]
             image = "kenwood/" + new_name + ":"+ tag
             cmd = "docker tag {0}   {1}".format(name, image)
-            subprocess.call("docker pull {}".format(name), shell=True)
+            subprocess.call("docker pull --platform linux/amd64 {}".format(name), shell=True)
             subprocess.run(["docker", "tag", name, image])
-            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker login -u johndai2024 -p Ecomol12", shell=True)
             subprocess.call("docker push {}".format(image), shell=True)
         else:
             new_name = "kenwood/" + name.split("/")[-1]
             cmd = "docker tag {0}   {1}".format(name, new_name)
-            subprocess.call("docker pull {}".format(name), shell=True)
+            subprocess.call("docker pull --platform linux/amd64 {}".format(name), shell=True)
             subprocess.run(["docker", "tag", name, new_name])
-            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker login -u johndai2024 -p Ecomol12", shell=True)
             subprocess.call("docker push {}".format(new_name), shell=True)
         
 if __name__ == "__main__":
